b1138e3877539cfe686335eb28a41b325813db24
Gaurav Jaiswal <101566310+Aryan-2-2@users.noreply.github.com>
2022-08-11 22:05:53 +0530

    bank account entity

--- Project unit-6/PaymentWalletApplication/src/main/java/com/masai/entities/BankAccount.java
+++ Project unit-6/PaymentWalletApplication/src/main/java/com/masai/entities/BankAccount.java
@@ -1,5 +1,40 @@
 package com.masai.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class BankAccount {
 
+	@Id
+	@Size(min = 1, message = "Bank Account Number must be minimum 1")
+	private Integer accountNo;
+	
+	@NotNull
+	@Pattern(regexp = "^[A-Z]{4}0[A-Z0-9]{6}")
+	private String ifscCode;
+	
+	@NotNull
+	@Size(min = 7, max = 20, message = "Bank Name must have min 7 and max 15 characters")
+	private String bankName;
+	
+	@NotNull
+	@Min(0)
+	private double balance;
+	
+	@ManyToOne
+	private Wallet wallet;	
 }
