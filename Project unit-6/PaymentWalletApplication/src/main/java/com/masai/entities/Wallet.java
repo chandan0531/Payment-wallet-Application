2123351b6db18394c1926b8152036c897dc8ae0b
sureshjungade <101568481+sureshjungade@users.noreply.github.com>
2022-08-12 11:03:18 +0530

    walletentitydone

--- Project unit-6/PaymentWalletApplication/src/main/java/com/masai/entities/Wallet.java
+++ Project unit-6/PaymentWalletApplication/src/main/java/com/masai/entities/Wallet.java
@@ -1,5 +1,29 @@
 package com.masai.entities;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Wallet {
 
+	@Id()
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer walletId;
+	@Min(value = 0, message="Balance Should be greater than zero")
+	@NotNull
+	private BigDecimal balance;
+	
 }
