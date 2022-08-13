7278d7826740d5929d6417acc8572f4b17b0f4e7
sureshjungade <101568481+sureshjungade@users.noreply.github.com>
2022-08-13 20:39:19 +0530

    entityvarabledefined

--- Project unit-6/PaymentWalletApplication/src/main/java/com/masai/entities/Wallet.java
+++ Project unit-6/PaymentWalletApplication/src/main/java/com/masai/entities/Wallet.java
@@ -1,13 +1,21 @@
 package com.masai.entities;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,5 +34,15 @@ public class Wallet {
 	@Min(value = 0, message="Balance Should be greater than zero")
 	@NotNull
 	private BigDecimal balance;
+	
+	@OneToMany
+	private List<BankAccount> bankAccount = new ArrayList<>();
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Customer customer;
+	
+	@JsonIgnore
+	@OneToMany
+	private List<Transaction> transactions;
 	
 }
--- Project unit-6/PaymentWalletApplication/src/main/java/com/masai/service/WalletServiceImpl.java
+++ Project unit-6/PaymentWalletApplication/src/main/java/com/masai/service/WalletServiceImpl.java
@@ -30,13 +30,7 @@ public class WalletServiceImpl implements WalletService{
 	@Autowired
 	private CustomerDao customerDao;
 	
-	@Override
-	public Customer createAccount(String name, String mobileno, BigDecimal amount) {
-		
-		
-		return null;
-	}
-
+	
 	@Override
 	public BigDecimal showBalance(String mobileNumber) {
 		
@@ -51,35 +45,29 @@ public class WalletServiceImpl implements WalletService{
 		
 	}
 
-	@Override
-	public Customer fundTransfer(String sourceMobileNo, String targetMobileNo, BigDecimal amount) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Customer depositAmount(String mobileno, BigDecimal amount) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Customer> getList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Customer updateAccount(Customer customer) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
 	public Customer addMoney(Wallet wallet, double amount) {
-		// TODO Auto-generated method stub
+		
+		
 		return null;
 	}
+
+
+	@Override
+	public Customer createAccount(Customer customer) {
+
+		Wallet wallet = customer.getWallet();
+		
+		walletDao.save(wallet);
+		
+		return customerDao.save(customer);
+	}
+
+	
+	
+	
+	
 
 	
 }
