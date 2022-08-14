e4f4a4b0929ab8823dfccdc68422f5b3d1947a16
alluShubham <Shubhamaryan857@gmail.com>
2022-08-14 23:13:00 +0530

    fix4

--- Project unit-6/PaymentWalletApplication/src/main/java/com/masai/service/BillPaymentServiceImpl.java
+++ Project unit-6/PaymentWalletApplication/src/main/java/com/masai/service/BillPaymentServiceImpl.java
@@ -1,23 +1,54 @@
 package com.masai.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.masai.entities.BillPayment;
+import com.masai.entities.Wallet;
 import com.masai.repository.BillPaymentDao;
+import com.masai.repository.WalletDao;
 
 @Service
 public class BillPaymentServiceImpl implements BillPaymentService {
 	
 	@Autowired
 	private BillPaymentDao billDao;
+	
+	@Autowired
+	private WalletDao wDao;
 
 	@Override
 	public BillPayment addBillPayment(BillPayment payment) {
 		
-		System.out.println("demo");
+		Integer wallId =10;
+//		Wallet wallet =payment.getWallet();//100
+//		Double amt = wallet.getBalance();
+		Double debitamt = payment.getAmount();
+		Wallet w1;
+		Double bal;
+		Optional<Wallet> opt = wDao.findById(wallId);
+		if(opt.isPresent()) {
+			 w1 =opt.get();
+			 bal =w1.getBalance();
+			if(bal>=debitamt) {
+				w1.setBalance(bal-debitamt);
+				wDao.save(w1);
+			}
+		}
+		else {
+			System.out.println("insuff");
+		}
 		
-		return null;
+		 
+		 billDao.save(payment);
+//		 System.out.println(payment.getWallet().getBalance());
+		
+		
+//		System.out.println("demo");
+		
+		return payment;
 	}
 
 	@Override
