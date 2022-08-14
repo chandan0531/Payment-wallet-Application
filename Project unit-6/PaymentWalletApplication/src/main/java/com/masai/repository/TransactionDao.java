313af367ba53231d95e89e59ff66af00c5538b0d
chandan0531 <chandan0521@gmail.com>
2022-08-14 21:15:11 +0530

    trans

--- Project unit-6/PaymentWalletApplication/src/main/java/com/masai/repository/TransactionDao.java
+++ Project unit-6/PaymentWalletApplication/src/main/java/com/masai/repository/TransactionDao.java
@@ -1,12 +1,18 @@
 package com.masai.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.masai.entities.Transaction;
+import com.masai.entities.Wallet;
 
 
 @Repository
 public interface TransactionDao extends JpaRepository<Transaction, Integer> {
-
+	
+	public List<Transaction> findByTransactionType(String transactionType);
+	
+	public List<Transaction> findByWallet(Wallet wallet);
 }
