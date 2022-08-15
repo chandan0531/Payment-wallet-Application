034c3959f3c40a9853f020877bcba8b0765d9aef
alluShubham <Shubhamaryan857@gmail.com>
2022-08-15 13:34:11 +0530

    fix5

--- Project unit-6/PaymentWalletApplication/src/main/java/com/masai/service/BillPaymentServiceImpl.java
+++ Project unit-6/PaymentWalletApplication/src/main/java/com/masai/service/BillPaymentServiceImpl.java
@@ -1,12 +1,16 @@
 package com.masai.service;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.masai.entities.BillPayment;
+import com.masai.entities.Transaction;
 import com.masai.entities.Wallet;
+import com.masai.exception.BillPaymentNotFoundException;
 import com.masai.repository.BillPaymentDao;
 import com.masai.repository.WalletDao;
 
@@ -14,16 +18,23 @@ import com.masai.repository.WalletDao;
 public class BillPaymentServiceImpl implements BillPaymentService {
 	
 	@Autowired
+	private WalletDao wDao;
+	
+	@Autowired
 	private BillPaymentDao billDao;
 	
 	@Autowired
-	private WalletDao wDao;
+	private TransactionServiceImpl trService;
 
 	@Override
-	public BillPayment addBillPayment(BillPayment payment) {
+	public BillPayment addBillPayment(BillPayment payment, Integer wallId) {
+		Wallet wallet =payment.getWallet();//100
 		
-		Integer wallId =10;
-//		Wallet wallet =payment.getWallet();//100
+		Transaction tr = new Transaction();
+		
+		tr.setAmount(payment.getAmount());
+		tr.setDescription(payment.getBillType());
+		tr.setTransactionDate(LocalDate.now());
 //		Double amt = wallet.getBalance();
 		Double debitamt = payment.getAmount();
 		Wallet w1;
@@ -35,26 +46,29 @@ public class BillPaymentServiceImpl implements BillPaymentService {
 			if(bal>=debitamt) {
 				w1.setBalance(bal-debitamt);
 				wDao.save(w1);
+				trService.addTansaction(tr);
 			}
-		}
+			}
 		else {
-			System.out.println("insuff");
+			throw new BillPaymentNotFoundException("Insufficient amount ");
 		}
 		
 		 
-		 billDao.save(payment);
-//		 System.out.println(payment.getWallet().getBalance());
-		
-		
-//		System.out.println("demo");
-		
-		return payment;
+		return  billDao.save(payment);
 	}
 
 	@Override
-	public BillPayment viewBillPayment(BillPayment payment) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<BillPayment> viewBillPayment(BillPayment payment, Integer wallId) {
+		Wallet w = payment.getWallet();
+		List<BillPayment> billList = w.getBillpayment();
+		if(billList.size()==0) {
+			throw new BillPaymentNotFoundException("No BillPaymets in the List ");
+		}
+		
+		
+//		https://us06web.zoom.us/j/4744923846?pwd=M2dxbkRxYXd0RlV1S0p1OHR2aTVXUT09
+		
+		return billList;
 	}
 
 }
