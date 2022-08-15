740702f1af75a54b36cc4999813d6ba3754277a6
sureshjungade <101568481+sureshjungade@users.noreply.github.com>
2022-08-15 15:28:54 +0530

    error3

--- Project unit-6/PaymentWalletApplication/src/main/java/com/masai/controller/BillPaymentController.java
+++ Project unit-6/PaymentWalletApplication/src/main/java/com/masai/controller/BillPaymentController.java
@@ -4,10 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.PathVariable;
-=======
->>>>>>> a79315d1c20025b14b6cbf462b02d566befcde87
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,15 +18,13 @@ public class BillPaymentController {
 	@Autowired
 	private BillPaymentService billPayService;
 	
-<<<<<<< HEAD
-	@PostMapping("/bills/{walletId}")
-	public BillPayment addBill(@RequestBody BillPayment pay,
-													@PathVariable("walletId") Integer wallId
-=======
+
+	
+
 	@PostMapping("/bills")
 	public BillPayment addBill(@RequestBody BillPayment pay,
 													@RequestBody Integer wallId
->>>>>>> a79315d1c20025b14b6cbf462b02d566befcde87
+
 			) {
 		return billPayService.addBillPayment(pay,wallId);
 	}
@@ -39,7 +34,6 @@ public class BillPaymentController {
 			@RequestBody Integer wallId
 			){
 		return billPayService.viewBillPayment(pay, wallId);
-		
 		
 	}
 
